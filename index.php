a4e5269a63a573f2f5e4cea9ee0d2e27ff8d709b
sainath g dutkar <sgd23@afsaccess1.njit.edu>
2017-10-16 20:33:23 -0400

    uploading the php files

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+//turn on debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+
+//instantiate the program object
+
+//Class to load classes it finds the file when the progrm starts to fail for calling a missing class
+class Manage {
+    public static function autoload($class) {
+        //you can put any file name or directory here
+        include $class . '.php';
+    }
+}
+
+spl_autoload_register(array('Manage', 'autoload'));
+
+//instantiate the program object
+$obj = new main();
+
+
+class main {
+    
+    public function __construct()
+    {
+        //print_r($_REQUEST);
+        //set default page request when no parameters are in URL
+        $pageRequest = 'homepage';
+        //check if there are parameters
+        if(isset($_REQUEST['page'])) {
+            //load the type of page the request wants into page request
+            $pageRequest = $_REQUEST['page'];
+        }
+        //instantiate the class that is being requested
+        $page = new $pageRequest;
+        
+        
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $page->get();
+        } else {
+            $page->post();
+        }
+        
+    }
+    
+}
+
+abstract class page {
+    protected $html;
+    
+    public function __construct()
+    {
+        $this->html .= '<html>';
+        $this->html .= '<link rel="stylesheet" href="styles.css">';
+        $this->html .= '<body>';
+    }
+    public function __destruct()
+    {
+        $this->html .= '</body></html>';
+        echo($this->html);
+    }
+    
+    public function get() {
+        echo 'default get message';
+    }
+    
+    public function post() {
+        print_r($_POST);
+    }
+}
+
+?>
