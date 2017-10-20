af4f3bb456692f5d38ab8dd8cd26c08069fdc079
sainath g dutkar <sgd23@afsaccess1.njit.edu>
2017-10-20 13:37:08 -0400

    updated to include all functionality under index file

--- index.php
+++ index.php
@@ -20,7 +20,6 @@ spl_autoload_register(array('Manage', 'autoload'));
 //instantiate the program object
 $obj = new main();
 
-
 class main {
     
     public function __construct()
@@ -48,7 +47,7 @@ class main {
 }
 
 abstract class page {
-    protected $html;
+   protected $html;
     
     public function __construct()
     {
@@ -70,5 +69,92 @@ abstract class page {
         print_r($_POST);
     }
 }
+
+
+class homepage extends page
+{
+    
+    public function get()
+    {
+        $form = '<form action="index.php?page=homepage" method="post" enctype="multipart/form-data">';
+        $form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
+        $form .= '<input type="submit" value="Upload CSV file" name="submit">';
+        $form .= '</form> ';
+        $this->html .= '<h1>Upload Form</h1>';
+        $this->html .= $form;
+        
+    }
+    
+    public function post(){
+        upload::uploadFile();
+    }
+    
+}
+
+
+class upload
+{
+     public static function uploadFile()
+    {
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["tmp_name"]);
+        $FileType = pathinfo($target_file,PATHINFO_EXTENSION);
+        // Check if file is a csv file
+        print_r($_FILES);
+        if(isset($_POST["submit"])) {
+            $fileName=$_FILES["fileToUpload"]["name"];
+            move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], 'uploads/' . $_FILES["fileToUpload"]["name"]);
+            header("Location: http://web.njit.edu/~sgd23/IS601101Project1/index.php?page=htmlTable&filename=".$_FILES["fileToUpload"]["name"]);
+        }
+    }
+}
+
+class htmlTable extends page {
+    
+    public function get(){
+        $csvFile=$_REQUEST["filename"];
+        $row = 1;
+        $tabledata = "";
+        if (($handle = fopen("uploads/".$csvFile, "r")) !== FALSE)
+        {
+            $tabledata .= '<table border="1">';
+            
+            while (($data = fgetcsv($handle)) !== FALSE) {
+                $num = count($data);
+                if ($row == 1) {
+                    $tabledata .= '<thead><tr>';
+                }else{
+                    $tabledata .= '<tr>';
+                }
+                
+                for ($c=0; $c < $num; $c++) {
+                    if(!isset($data[$c])) {
+                        $value = "&nbsp;";
+                    }else{
+                        $value = $data[$c];
+                    }
+                    if ($row == 1) {
+                        $tabledata .= '<th>'.$value.'</th>';
+                    }else{
+                        $tabledata .= '<td>'.$value.'</td>';
+                    }
+                }
+                
+                if ($row == 1) {
+                    $tabledata .= '</tr></thead><tbody>';
+                }else{
+                    $tabledata .= '</tr>';
+                }
+                $row++;
+            }
+            
+            $tabledata .= '</tbody></table>';
+            fclose($handle);
+            $this->html .= $tabledata;
+        }
+    }
+}
+
+
 
 ?>
